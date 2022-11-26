3832a6432ff748c6cd6cbc18665cd08bf67f3022
ProgrammingCode-gif <102915411+ProgrammingCode-gif@users.noreply.github.com>
2022-11-26 16:14:53 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,71 @@
+let a = '';
+let b = '';
+let sign = '';
+let finish = false;
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'X', '/']
+
+const out = document.querySelector('.calc-screen p')
+
+function clearAll () {
+    a = '';
+    b = '';
+    sign = '';
+    finish = false;
+    out.textContent = 0;
+}
+
+document.querySelector('.ac').onclick = clearAll;
+
+document.querySelector('.buttons').onclick = (event) => {
+    if(!event.target.classList.contains('btn')) return;
+    if(event.target.classList.contains('ac')) return;
+
+    out.textContent = '';
+    const key = event.target.textContent
+    if(digit.includes(key)) {
+        if(b === '' && sign === '') {
+            a += key
+            out.textContent = a
+        }else if(a !== '' && sign !== '' && finish){
+            b = key;
+            finish = false;
+            out.textContent = b;
+        }else {
+            b += key
+            out.textContent = b;
+        }
+        return;
+    }
+    if(action.includes(key)) {
+        sign = key
+        out.textContent = sign;
+    }
+    if(key === '=') {
+        if(b === '') b = a;
+        switch (sign) {
+            case '+':
+                a = (+a) + (+b);
+                break;
+            case '-':
+                a = (+a) - (+b);
+                break;
+            case 'X':
+                a = a * b;
+                break;
+            case '/':
+                if(b === '0') {
+                    out.textContent = 'Ошибка';
+                    a = '';
+                    b = '';
+                    sign = '';
+                    return
+                }
+                a = a / b;
+                break;
+        }
+        finish = true;
+        out.textContent = a;
+    }
+}
